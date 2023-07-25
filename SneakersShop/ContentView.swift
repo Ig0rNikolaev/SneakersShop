4a81a7932085082f3e3ffb087bcee79edd4de270
Igor Nikolaev <igor01131987@gmail.com>
2023-07-25 21:11:50 +0300

    Add authorization screen elements.

--- SneakersShop/ContentView.swift
+++ SneakersShop/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("Авторизация")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title2.bold())
+                .background(Color("whiteAlfa"))
+                .cornerRadius(30)
+                .padding(.bottom, 30)
+                .foregroundColor(Color("darkBrown"))
+
+            VStack {
+                TextField("Введите Email", text: $email)
+                    .padding()
+                    .background(Color("whiteAlfa"))
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+
+                SecureField("Введите пароль", text: $password)
+                    .padding()
+                    .background(Color("whiteAlfa"))
+                    .cornerRadius(12)
+                    .padding(8)
+                    .padding(.horizontal, 12)
+
+                Button {
+                    print("Войти")
+                } label: {
+                    Text("Войти")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(colors: [Color(.systemYellow), Color(.systemOrange)], startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(Color("darkBrown"))
+                }
+
+                Button {
+                    print("Еще не с нами?")
+                } label: {
+                    Text("Еще не с нами?")
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(12)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(Color("darkBrown"))
+                }
+
+            }
+            .padding()
+            .padding(.top, 15)
+            .background(Color("whiteAlfa"))
+            .cornerRadius(25)
+            .padding(30)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Image("bg")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea())
     }
 }
 
