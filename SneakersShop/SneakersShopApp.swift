02b156215c875e9a1993c3f490a5c803018383ee
Igor Nikolaev <igor01131987@gmail.com>
2023-07-25 16:10:45 +0300

    Initial Commit

---
+++ SneakersShop/SneakersShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakersShopApp.swift
+//  SneakersShop
+//
+//  Created by Игорь Николаев on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SneakersShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
